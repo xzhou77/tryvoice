ac6980b72424ac8d98d8dafa7265859d13a182fb
xzhou77 <xzhou77@gmail.com>
2023-06-03 12:27:28 +0800

    modify

--- index.js
+++ index.js
@@ -66,10 +66,10 @@ recognition.onresult = async (e) => {
         case transcript.indexOf("groceries") > -1: utter.text = "Sure, what do we need to buy ? "; break;
         case transcript.indexOf("milk") > -1: utter.text = "Let's confirm 1 litre of milk, 1 pack of bread, and 2 cucumbers."; break;
         case transcript.indexOf("yes confirm") > -1:
-        case transcript.indexOf("Yes, thanks") > -1:  utter.text = "Ok, got it."; break;
+        case transcript.indexOf("Yes, confirm") > -1:  utter.text = "Ok, got it."; break;
         case transcript.indexOf("lunch") > -1: utter.text = "Sure, what do you want for your lunch ? Noodle, burger, or fried rice. "; break;
         case transcript.indexOf("fish") > -1: utter.text = "Ok, I will get the fish soup noodle for you."; break;
-        case transcript.indexOf("around 2:00pm") > -1:
+        case transcript.indexOf("around 2:00") > -1:
         case transcript.indexOf("Around 2:00pm") > -1: utter.text = "Ok, I will make a booking to pick us at 2pm today. "; break;
         case transcript.indexOf("Club") > -1: utter.text = "Yes, what time do you want to go to Island Club ?"; break;
         case transcript.indexOf("你好") > -1: utter.text = "哈喽， 你好!"; break;
